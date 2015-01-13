7cd827e56b91497954c1d76858475ebb07760b10
Pham Minh Quan <dreamtheater2195@gmail.com>
2015-01-13 16:58:31 +0700

    Add gravatar image and a sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    #debugger #byebug gem
+  end
 end
+#Whenever you’re confused about something in a Rails application, it’s a good practice to put
+#debugger close to the code you think might be causing the trouble. Inspecting the state of
+#the system using byebug is a powerful method for tracking down application errors and
+#interactively debugging your application.
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false } #Tom and tom is similar
   has_secure_password
   validates :password, length: { minimum: 6 }
 end
