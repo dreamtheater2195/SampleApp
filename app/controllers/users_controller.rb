97051eeff2a9e02b861905253c1c8665dfc7e8a6
Pham Minh Quan <dreamtheater2195@gmail.com>
2015-02-01 23:27:09 +0700

    Finish user edit,update,index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
     @user = User.new
@@ -21,6 +28,42 @@ class UsersController < ApplicationController
     #debugger #byebug gem
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Update completed!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 #Whenever you’re confused about something in a Rails application, it’s a good practice to put
 #debugger close to the code you think might be causing the trouble. Inspecting the state of
 #the system using byebug is a powerful method for tracking down application errors and
@@ -29,5 +72,4 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
-
 end
